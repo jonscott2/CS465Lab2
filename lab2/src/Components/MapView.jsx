8a1bf195a5ebdfbf812653f3bea84c9449531ec9
jonscott2 <jonathan.scott6@snhu.edu>
2025-10-06 19:54:13 -0400

    leaflet
    - imported leaflet
    - fit to screen
    - added basic marker onclick method

---
+++ lab2/src/Components/MapView.jsx
@@ -0,0 +1,62 @@
+// components/MapView.jsx
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+// Component to handle map clicks
+function ClickHandler({ setMarkerPosition }) {
+  useMapEvents({
+    click(e) {
+      setMarkerPosition(e.latlng);
+      console.log('Map clicked at:', e.latlng); // For debugging
+    },
+  });
+  return null;
+}
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+const MapView = () => {
+    const [markerPosition, setMarkerPosition] = useState(null);
+
+  return (
+    <MapContainer center={[43.1939, -71.5724]} zoom={13} scrollWheelZoom={true} style={{ height: '40vw', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {/* <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty popup. <br /> Easily customizable.
+        </Popup>
+      </Marker> */}
+
+      {/* Attach click handler */}
+      <ClickHandler setMarkerPosition={setMarkerPosition} />
+
+      {/* Conditionally render marker */}
+      {markerPosition && (
+        <Marker position={markerPosition}>
+          <Popup>
+            You clicked here! <br />
+            Lat: {markerPosition.lat.toFixed(4)}, Lng: {markerPosition.lng.toFixed(4)}
+          </Popup>
+        </Marker>
+      )}
+      
+    </MapContainer>
+  );
+};
+
+export default MapView;
